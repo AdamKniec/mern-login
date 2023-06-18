8bcdb37113780d3c3924d3a0cb6b3067c8396959
AdamKniec <adam.k.kniec@gmail.com>
2023-06-18 13:52:21 +0200

    adding the password hash functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+import User from "../models/userModel.js";
+
 const authenticateUser = (req, res) => {
   res.status(200).json({ response: "Response from the backend server" });
 };
@@ -13,8 +15,28 @@ const updateProfile = (req, res) => {
   res.status(200).json({ response: "User's profile update" });
 };
 
-const registerUser = (req, res) => {
-  res.status(200).json({ response: "User registered!" });
+const registerUser = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ message: "User exists in the DB!" });
+    }
+  } catch (err) {
+    res.status(500);
+    throw new Error("There was a problem with authenticating");
+  }
+
+  const user = new User({
+    email: email,
+    password: password,
+    name: name,
+  });
+
+  if (user) {
+    await User.create(user);
+    res.status(201).json(user);
+  }
 };
 export {
   authenticateUser,
